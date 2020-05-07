27d008b7ece551dfa0c2ebe4fde2d11f68c79cd0
emil vaklinov <emil_vaklinov@yahoo.com>
2020-05-07 11:59:43 +0100

    secondary button created

---
+++ Sources/FABButton/FABInsetLabel.swift
@@ -0,0 +1,35 @@
+//
+//  FABInsetLabel.swift
+//  
+//
+//  Created by Emil Vaklinov on 07/05/2020.
+//
+
+import UIKit
+
+class FABInsetLabel: UILabel {
+
+    var contentInsets = UIEdgeInsets.zero
+
+    override func drawText(in rect: CGRect) {
+        let insetRect = rect.inset(by: contentInsets)
+        super.drawText(in: insetRect)
+    }
+
+    
+    override var intrinsicContentSize: CGSize {
+        return addInsets(to: super.intrinsicContentSize)
+    }
+
+    
+    override func sizeThatFits(_ size: CGSize) -> CGSize {
+        return addInsets(to: super.sizeThatFits(size))
+    }
+
+    
+    private func addInsets(to size: CGSize) -> CGSize {
+        let width = size.width + contentInsets.left + contentInsets.right
+        let height = size.height + contentInsets.top + contentInsets.bottom
+        return CGSize(width: width, height: height)
+    }
+}
